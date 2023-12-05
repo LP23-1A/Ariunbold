3689994401254486b59fbea17eaeae3418ccebbc
raz11244 <invo.wizard22@gmail.com>
2023-12-05 09:55:58 +0800

    all blog iin comp duusaw

--- blog/src/components/AllBlogCategory.jsx
+++ blog/src/components/AllBlogCategory.jsx
@@ -1,6 +1,6 @@
 export default function AllBlogCategory(){
     return (
-        <div className="w-fit py-[4px] px-[10px] flex justify-center items-center font-14-blue rounded-[6px] bg-[grey]">
+        <div className="w-fit py-[4px] px-[10px] flex justify-center items-center font-14-blue rounded-[6px] bg-[#4b6bfb0d]">
             Technology
         </div>
     )
