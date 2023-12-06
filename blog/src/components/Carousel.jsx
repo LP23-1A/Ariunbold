3b3f9ec884638ff5d3251210de9a0160cb1260f4
raz11244 <invo.wizard22@gmail.com>
2023-12-06 10:57:15 +0800

    carousel duusaw

--- blog/src/components/Carousel.jsx
+++ blog/src/components/Carousel.jsx
@@ -5,7 +5,8 @@ import RightArrow from "./icons/RightArrow";
 export default function Carousel(){
     return (
         <div className="relative flex flex-col gap-[5px] justify-center">
-            <img className="w-[1216px] h-[600px] rounded-[12px] object-cover" src="https://s3-alpha-sig.figma.com/img/eb4f/aad2/4394e91108e011b0d07581596959713b?Expires=1702857600&Signature=TEJqgZebhD5ngGJk43qte2~qf859bguz1g9w0nc-t-7JwZKqGO9HkQ4f7UsByRTKRdqOFoXLq-GeNWVTxyF9yaI-X8jNnKzpHHPB0mwm5217XLBDvleUnawK8Q52jyuf37X~tBpedwk-oALlTZsspRn-XWzxdTRQOMwtXGLvj~Kv~0VS~Cxf7gUiI44x1oZGIcO9HQbL7oAr57JaTLqKz-5STAIIaCV4bEzUKUSd3x8o89LHrwfVdB1rnbaOm-NrwsFjsCGap2EeymdGc1fM9MLK7cYBadPJIs062VLD9cvqO4LZZ7kzDuD2FWVmWuhODVPANFGn0YJgP78jzOayAg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
+            <img className="w-[1216px] h-[600px] rounded-[12px] object-cover relative" src="https://s3-alpha-sig.figma.com/img/eb4f/aad2/4394e91108e011b0d07581596959713b?Expires=1702857600&Signature=TEJqgZebhD5ngGJk43qte2~qf859bguz1g9w0nc-t-7JwZKqGO9HkQ4f7UsByRTKRdqOFoXLq-GeNWVTxyF9yaI-X8jNnKzpHHPB0mwm5217XLBDvleUnawK8Q52jyuf37X~tBpedwk-oALlTZsspRn-XWzxdTRQOMwtXGLvj~Kv~0VS~Cxf7gUiI44x1oZGIcO9HQbL7oAr57JaTLqKz-5STAIIaCV4bEzUKUSd3x8o89LHrwfVdB1rnbaOm-NrwsFjsCGap2EeymdGc1fM9MLK7cYBadPJIs062VLD9cvqO4LZZ7kzDuD2FWVmWuhODVPANFGn0YJgP78jzOayAg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
+            <div className="w-[1216px] h-[600px] rounded-[12px] bg-[#141624] opacity-[40%] top-0 absolute"></div>
             <div className="rounded-[12px] flex flex-col gap-[24px] p-[40px] w-[598px] bg-white absolute bottom-[55px] left-[10px]">
                 <div className="flex flex-col gap-[16px]">
                     <TrendingCategory/>
--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -4,24 +4,10 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import TrendingNews from "@/components/TrendingNews";
 
-function axiosTest() {
-  const promise = axios.get('https://dev.to/api/articles')
-
-  const dataPromise = promise.then((response) => response.data)
-
-  return dataPromise
-}
-
-axiosTest()
-  .then(data => {
-      response.json({ message: 'Request received!', data })
-  })
-  .catch(err => console.log(err))
-
 
 export default function Home(){
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center gap-[100px] items-center">
       <Navbar/>
       <Carousel/>
       <Footer/>
