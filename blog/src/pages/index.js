d0e77515d4043fd77d9ff52ad6d03d1dc6a5658f
raz11244 <invo.wizard22@gmail.com>
2023-12-06 10:41:39 +0800

    main ruu

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -4,6 +4,21 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import TrendingNews from "@/components/TrendingNews";
 
+function axiosTest() {
+  const promise = axios.get('https://dev.to/api/articles')
+
+  const dataPromise = promise.then((response) => response.data)
+
+  return dataPromise
+}
+
+axiosTest()
+  .then(data => {
+      response.json({ message: 'Request received!', data })
+  })
+  .catch(err => console.log(err))
+
+
 export default function Home(){
   return (
     <div className="flex flex-col justify-center items-center">
